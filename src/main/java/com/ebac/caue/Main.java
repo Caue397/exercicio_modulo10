999a5066b3575b6b9707ec27eb22ca1c2d0af843
Cauê Gallizzi <cauegallizzi397@gmail.com>
2024-10-28 09:58:41 -0300

    fix

--- src/main/java/com/ebac/caue/Main.java
+++ src/main/java/com/ebac/caue/Main.java
@@ -8,35 +8,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a nota do aluno: ");
-        int nota = scanner.nextInt();
-        String resultado = verificaNota(nota);
+        System.out.println("Digite as quatro notas: ");
+        double nota1 = scanner.nextDouble();
+        double nota2 = scanner.nextDouble();
+        double nota3 = scanner.nextDouble();
+        double nota4 = scanner.nextDouble();
+        double mediaFinal = calculaMedia(nota1, nota2, nota3, nota4);
+        String resultado = verificaMedia(mediaFinal);
         System.out.println(resultado);
     }
 
-    public static String verificaNota(int nota) {
-        String resultado;
-        switch (nota) {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-                resultado = "Aluno reprovado";
-                break;
-            case 5:
-            case 6:
-                resultado = "Aluno de recuperação";
-                break;
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-                resultado = "Aluno aprovado";
-                break;
-            default:
-                resultado = "Nota inválida. Digite um número de 0 a 10.";
+    public static double calculaMedia(double nota1, double nota2, double nota3, double nota4) {
+        return (nota1 + nota2 + nota3 + nota4) / 4;
+    }
+
+    // Não utilizei if, pois o 'SwitchCase' não suporta o tipo double
+    public static String verificaMedia(double nota) {
+        if (nota > 10 || nota < 0) {
+            return "Nota inválida. Digite uma nota de 0 a 10";
+        } else if (nota >= 7) {
+            return "Aluno aprovado";
+        } else if (nota >= 5) {
+            return "Aluno de recuperação";
+        } else {
+            return "Aluno reprovado";
         }
-        return resultado;
     }
 }
